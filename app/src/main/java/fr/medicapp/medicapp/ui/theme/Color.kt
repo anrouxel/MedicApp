e9cb04138d6c69a41f0ae004bd8a7bb8aa88b933
Antonin Rouxel <antonin.rouxel44@outlook.fr>
2023-10-12 21:24:09 +0200

    Add red color

--- app/src/main/java/fr/medicapp/medicapp/ui/theme/Color.kt
+++ app/src/main/java/fr/medicapp/medicapp/ui/theme/Color.kt
@@ -10,7 +10,7 @@ val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
 
-
+// Caribbean Green
 val CaribbeanGreen50 = Color(0xFFeafff6)
 val CaribbeanGreen100 = Color(0xFFcdfee8)
 val CaribbeanGreen200 = Color(0xFFa0fad6)
@@ -22,3 +22,16 @@ val CaribbeanGreen700 = Color(0xFF008363)
 val CaribbeanGreen800 = Color(0xFF00674f)
 val CaribbeanGreen900 = Color(0xFF005542)
 val CaribbeanGreen950 = Color(0xFF003027)
+
+// Red Ribbon
+val RedRibbon50 = Color(0xFFffeff1)
+val RedRibbon100 = Color(0xFFffe0e5)
+val RedRibbon200 = Color(0xFFffc6d2)
+val RedRibbon300 = Color(0xFFff97ac)
+val RedRibbon400 = Color(0xFFff5d81)
+val RedRibbon500 = Color(0xFFff245a)
+val RedRibbon600 = Color(0xFFef0044)
+val RedRibbon700 = Color(0xFFd7003d)
+val RedRibbon800 = Color(0xFFb4003c)
+val RedRibbon900 = Color(0xFF99023a)
+val RedRibbon950 = Color(0xFF57001a)
