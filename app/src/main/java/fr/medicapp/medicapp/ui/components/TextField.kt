9b4f6a19a686c8f5330f64990cf6461a843872f8
Antonin Rouxel <antonin.rouxel44@outlook.fr>
2023-10-12 18:28:35 +0200

    Add TextField

---
+++ app/src/main/java/fr/medicapp/medicapp/ui/components/TextField.kt
@@ -0,0 +1,38 @@
+package fr.medicapp.medicapp.ui.components
+
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.tooling.preview.Preview
+import fr.medicapp.medicapp.ui.theme.CaribbeanGreen500
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun MedicAppTextField() {
+    var text by rememberSaveable {
+        mutableStateOf("")
+    }
+
+    OutlinedTextField(
+        value = text,
+        onValueChange = { text = it},
+        label = { Text("Email") },
+        colors = TextFieldDefaults.outlinedTextFieldColors(
+            focusedBorderColor = CaribbeanGreen500,
+            unfocusedBorderColor = CaribbeanGreen500,
+        )
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun TextFieldPreview() {
+    MedicAppTextField()
+}
