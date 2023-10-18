5c697d3a9abcf0b8a1cc2f28fba2c24931e29439
Antonin Rouxel <antonin.rouxel44@outlook.fr>
2023-10-18 23:02:37 +0200

    Change Navigation bar to Navigation Drawer

--- app/src/main/java/fr/medicapp/medicapp/ui/home/Home.kt
+++ app/src/main/java/fr/medicapp/medicapp/ui/home/Home.kt
@@ -2,6 +2,7 @@ package fr.medicapp.medicapp.ui.home
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -12,21 +13,30 @@ import androidx.compose.material.icons.filled.AccountCircle
 import androidx.compose.material.icons.filled.Home
 import androidx.compose.material.icons.filled.MedicalInformation
 import androidx.compose.material.icons.filled.Medication
+import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material.icons.filled.Message
+import androidx.compose.material.icons.filled.Newspaper
 import androidx.compose.material.icons.filled.Notifications
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.DrawerValue
 import androidx.compose.material3.ElevatedCard
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.ModalDrawerSheet
+import androidx.compose.material3.ModalNavigationDrawer
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.NavigationDrawerItem
+import androidx.compose.material3.NavigationDrawerItemDefaults
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.rememberDrawerState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
@@ -35,134 +45,198 @@ import androidx.compose.ui.unit.dp
 import fr.medicapp.medicapp.R
 import fr.medicapp.medicapp.ui.theme.EUGreen100
 import fr.medicapp.medicapp.ui.theme.EUGreen120
+import kotlinx.coroutines.launch
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun Home() {
-    Scaffold(
-        modifier = Modifier.fillMaxSize(),
-        topBar = {
-            CenterAlignedTopAppBar(
-                title = {
-                    Image(
-                        painter = painterResource(id = R.drawable.ic_launcher_foreground),
-                        contentDescription = "MedicApp"
-                    )
-                },
-                actions = {
-                    IconButton(onClick = { /*TODO*/ }) {
-                        Icon(
-                            imageVector = Icons.Filled.Notifications,
-                            contentDescription = "Notifications"
-                        )
-                    };
-                    IconButton(onClick = { /*TODO*/ }) {
-                        Icon(
-                            imageVector = Icons.Filled.AccountCircle,
-                            contentDescription = "Account"
-                        )
-                    }
-                }
-            )
-        },
-        bottomBar = {
-            NavigationBar {
-                NavigationBarItem(
-                    selected = true,
-                    onClick = { /*TODO*/ },
+    val drawerState = rememberDrawerState(initialValue = DrawerValue.Closed)
+    val scope = rememberCoroutineScope()
+    ModalNavigationDrawer(
+        drawerState = drawerState,
+        drawerContent = {
+            ModalDrawerSheet {
+                Spacer(modifier = Modifier.height(12.dp))
+                NavigationDrawerItem(
                     icon = {
                         Icon(
                             imageVector = Icons.Filled.Home,
-                            contentDescription = "Home"
+                            contentDescription = "Accueil"
                         )
-                    }
-                );
-                NavigationBarItem(
-                    selected = false,
-                    onClick = { /*TODO*/ },
+                    },
+                    label = {
+                        Text(text = "Accueil")
+                    },
+                    selected = true,
+                    onClick = {
+                        scope.launch {
+                            drawerState.close()
+                        }
+                    },
+                    modifier = Modifier.padding(NavigationDrawerItemDefaults.ItemPadding)
+                )
+                NavigationDrawerItem(
                     icon = {
                         Icon(
                             imageVector = Icons.Filled.MedicalInformation,
                             contentDescription = "Ordonnances"
                         )
                     },
-                );
-                NavigationBarItem(
+                    label = {
+                        Text(text = "Ordonnances")
+                    },
                     selected = false,
-                    onClick = { /*TODO*/ },
+                    onClick = {
+                        scope.launch {
+                            drawerState.close()
+                        }
+                    },
+                    modifier = Modifier.padding(NavigationDrawerItemDefaults.ItemPadding)
+                )
+                NavigationDrawerItem(
                     icon = {
                         Icon(
                             imageVector = Icons.Filled.Medication,
                             contentDescription = "Medicaments"
                         )
-                    }
-                );
-                NavigationBarItem(
+                    },
+                    label = {
+                        Text(text = "Medicaments")
+                    },
                     selected = false,
-                    onClick = { /*TODO*/ },
+                    onClick = {
+                        scope.launch {
+                            drawerState.close()
+                        }
+                    },
+                    modifier = Modifier.padding(NavigationDrawerItemDefaults.ItemPadding)
+                )
+                NavigationDrawerItem(
+                    icon = {
+                        Icon(
+                            imageVector = Icons.Filled.Newspaper,
+                            contentDescription = "Effets indesirables"
+                        )
+                    },
+                    label = {
+                        Text(text = "Effets indesirables")
+                    },
+                    selected = false,
+                    onClick = {
+                        scope.launch {
+                            drawerState.close()
+                        }
+                    },
+                    modifier = Modifier.padding(NavigationDrawerItemDefaults.ItemPadding)
+                )
+                NavigationDrawerItem(
                     icon = {
                         Icon(
                             imageVector = Icons.Filled.Message,
-                            contentDescription = "Messagerie"
+                            contentDescription = "Messages"
                         )
-                    }
+                    },
+                    label = {
+                        Text(text = "Messages")
+                    },
+                    selected = false,
+                    onClick = {
+                        scope.launch {
+                            drawerState.close()
+                        }
+                    },
+                    modifier = Modifier.padding(NavigationDrawerItemDefaults.ItemPadding)
                 )
             }
         }
-    ) { innerPadding ->
-        Column(
-            modifier = Modifier
-                .fillMaxSize()
-                .padding(innerPadding)
-                .padding(10.dp),
-        ) {
-            ElevatedCard(
-                onClick = { /*TODO*/ },
-                elevation = CardDefaults.cardElevation(
-                    defaultElevation = 6.dp
-                ),
+    ) {
+        Scaffold(
+            modifier = Modifier.fillMaxSize(),
+            topBar = {
+                CenterAlignedTopAppBar(
+                    title = {
+                        Image(
+                            painter = painterResource(id = R.drawable.ic_launcher_foreground),
+                            contentDescription = "MedicApp"
+                        )
+                    },
+                    navigationIcon = {
+                        IconButton(onClick = {
+                            scope.launch {
+                                drawerState.open()
+                            }
+                        }) {
+                            Icon(
+                                imageVector = Icons.Filled.Menu,
+                                contentDescription = "Menu"
+                            )
+                        }
+                    },
+                    actions = {
+                        IconButton(onClick = { /*TODO*/ }) {
+                            Icon(
+                                imageVector = Icons.Filled.Notifications,
+                                contentDescription = "Notifications"
+                            )
+                        };
+                    }
+                )
+            },
+        ) { innerPadding ->
+            Column(
                 modifier = Modifier
-                    .fillMaxWidth()
-                    .height(height = 200.dp),
-                colors = CardDefaults.cardColors(
-                    containerColor = EUGreen120,
-                    contentColor = Color.White
-                ),
-                shape = RoundedCornerShape(10.dp)
+                    .fillMaxSize()
+                    .padding(innerPadding)
+                    .padding(10.dp),
             ) {
-                Column(
+                ElevatedCard(
+                    onClick = { /*TODO*/ },
+                    elevation = CardDefaults.cardElevation(
+                        defaultElevation = 6.dp
+                    ),
                     modifier = Modifier
-                        .fillMaxSize()
-                        .padding(10.dp),
+                        .fillMaxWidth()
+                        .height(height = 200.dp),
+                    colors = CardDefaults.cardColors(
+                        containerColor = EUGreen120,
+                        contentColor = Color.White
+                    ),
+                    shape = RoundedCornerShape(10.dp)
                 ) {
-                    Text(
-                        text = "Bonjour, John Doe",
-                    )
+                    Column(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .padding(10.dp),
+                    ) {
+                        Text(
+                            text = "Bonjour, John Doe",
+                        )
+                    }
                 }
-            }
-            Button(
-                onClick = { /*TODO*/ },
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(top = 10.dp, bottom = 0.dp),
-                colors = ButtonDefaults.buttonColors(
-                    containerColor = EUGreen100
-                ),
-                shape = RoundedCornerShape(10.dp)
-            ) {
-                Text(text = "Ajouter une ordonnance")
-            }
-            Button(
-                onClick = { /*TODO*/ },
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(top = 10.dp, bottom = 0.dp),
-                colors = ButtonDefaults.buttonColors(
-                    containerColor = EUGreen100
-                ),
-                shape = RoundedCornerShape(10.dp)
-            ) {
-                Text(text = "Signaler un effet indesirable")
+                Button(
+                    onClick = { /*TODO*/ },
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(top = 10.dp, bottom = 0.dp),
+                    colors = ButtonDefaults.buttonColors(
+                        containerColor = EUGreen100
+                    ),
+                    shape = RoundedCornerShape(10.dp)
+                ) {
+                    Text(text = "Ajouter une ordonnance")
+                }
+                Button(
+                    onClick = { /*TODO*/ },
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(top = 10.dp, bottom = 0.dp),
+                    colors = ButtonDefaults.buttonColors(
+                        containerColor = EUGreen100
+                    ),
+                    shape = RoundedCornerShape(10.dp)
+                ) {
+                    Text(text = "Signaler un effet indesirable")
+                }
             }
         }
     }
