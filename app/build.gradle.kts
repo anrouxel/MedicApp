2a28a5875fa696cda842662fe4b5344ea3cec7cd
Antonin Rouxel <antonin.rouxel44@outlook.fr>
2023-10-18 21:24:47 +0200

    Add ACCOMPANIST

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -63,6 +63,9 @@ dependencies {
     // Material Design icons
     implementation("androidx.compose.material:material-icons-extended")
 
+    // Accompanist
+    implementation("com.google.accompanist:accompanist-permissions:0.32.0")
+
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
