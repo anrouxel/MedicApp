39cf8981ffae679a13d7f415d1620cdfb5427ad1
Arthur OSSELIN <arthur.osselin@etu.univ-nantes.fr>
2024-01-10 11:45:02 +0100

    Beginning of tests and sample data

---
+++ app/src/test/java/fr/medicapp/medicapp/repository/DurationRepositoryTest.kt
@@ -0,0 +1,23 @@
+package fr.medicapp.medicapp.repository
+
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+
+class DurationRepositoryTest {
+
+    @Test
+    fun getDurationAll() {
+    }
+
+    @Test
+    fun addDuration() {
+    }
+
+    @Test
+    fun deleteDuration() {
+    }
+
+    @Test
+    fun updateDuration() {
+    }
+}
---
+++ app/src/test/java/fr/medicapp/medicapp/repository/GenericGroupRepositoryTest.kt
@@ -0,0 +1,27 @@
+package fr.medicapp.medicapp.repository
+
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+
+class GenericGroupRepositoryTest {
+
+    @Test
+    fun getGenericGroupAll() {
+    }
+
+    @Test
+    fun getGenericGroupOne() {
+    }
+
+    @Test
+    fun addGenericGroup() {
+    }
+
+    @Test
+    fun deleteGenericGroup() {
+    }
+
+    @Test
+    fun updateGenericGroup() {
+    }
+}
