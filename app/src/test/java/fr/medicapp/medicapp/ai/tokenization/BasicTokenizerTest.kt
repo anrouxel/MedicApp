8c57a34f6a415860634a53f372fd5c32077517d4
E211439A <L4v3r1T3>
2024-01-12 09:41:36 +0100

    BasicTokenizerTest fait + DoctorRepositoryTest mis en commentaire

--- app/src/test/java/fr/medicapp/medicapp/ai/tokenization/BasicTokenizerTest.kt
+++ app/src/test/java/fr/medicapp/medicapp/ai/tokenization/BasicTokenizerTest.kt
@@ -1,6 +1,7 @@
 package fr.medicapp.medicapp.ai.tokenization
 
 import fr.medicapp.medicapp.tokenization.BasicTokenizer
+import org.junit.Assert.assertEquals
 import org.junit.Test
 
 class BasicTokenizerTest {
@@ -8,8 +9,27 @@ class BasicTokenizerTest {
     val basicTokenizer = BasicTokenizer()
 
     @Test
-    fun `testTokenize`() {
+    fun `test tokenize valide en français`() {
         val response = basicTokenizer.tokenize("Je suis une loutre.")
-        assert(response== mutableListOf<String>("Je","suis","une","loutre","."))
+        assertEquals(mutableListOf<String>("Je","suis","une","loutre","."),response)
     }
+
+    @Test
+    fun `test tokenize valide en anglais`() {
+        val response = basicTokenizer.tokenize("This is in English.")
+        assertEquals(mutableListOf<String>("This","is","in","English","."),response)
+    }
+
+    @Test
+    fun `test tokenize en français avec des mots inconnus`() {
+        val response = basicTokenizer.tokenize("Ceci est gjaeorgn.")
+        assertEquals(mutableListOf<String>("Ceci","est","gjaeorgn","."), response)
+    }
+
+    @Test
+    fun `test tokenize string vide`() {
+        val response = basicTokenizer.tokenize("")
+        assertEquals(mutableListOf<String>(), response)
+    }
+
 }
