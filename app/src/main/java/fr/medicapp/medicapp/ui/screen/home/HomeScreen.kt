6248fdb5768635134fdca517cb7cbfa580442f76
E213909K <lucas.payet@etu.univ-nantes.fr>
2024-02-19 10:15:46 +0100

    removed one of the two ListOfMedication. My life is misery

--- app/src/main/java/fr/medicapp/medicapp/ui/screen/home/HomeScreen.kt
+++ app/src/main/java/fr/medicapp/medicapp/ui/screen/home/HomeScreen.kt
@@ -5,8 +5,6 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -16,14 +14,11 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import fr.medicapp.medicapp.database.ObjectBox
 import fr.medicapp.medicapp.database.entity.PrescriptionEntity
-import fr.medicapp.medicapp.model.Prescription
-import fr.medicapp.medicapp.ui.components.button.ReusableElevatedCardButton
 import fr.medicapp.medicapp.ui.components.calendar.Calendar
-import fr.medicapp.medicapp.ui.components.card.CardContent
+import fr.medicapp.medicapp.ui.components.list.ListOfMedication
 import fr.medicapp.medicapp.ui.theme.EUPurpleColorShema
 import fr.medicapp.medicapp.ui.theme.MedicAppTheme
 import java.time.LocalDate
-import java.time.LocalDateTime
 
 /**
  * Écran d'accueil de l'application MedicApp.
@@ -57,29 +52,6 @@ fun HomeScreen() {
     }
 }
 
-@Composable
-fun ListOfMedication(
-    modifier: Modifier = Modifier,
-    selectedDate: LocalDate,
-    prescription: List<Prescription>
-) {
-
-    val medocsForToday = prescription
-        .map { it.getNotificationsDates(selectedDate) }
-        .flatten()
-
-
-    LazyColumn(modifier = modifier) {
-        items(medocsForToday) {
-            val name = it.prescription.treatment.medication.toString()
-            val hourAndMinute = "${it.date.hour}h${it.date.minute.toString().padStart(2, '0')}"
-            val enabled = it.date.isAfter(LocalDateTime.now())
-            ReusableElevatedCardButton(enabled = enabled, onClick = {}) {
-                CardContent(title = name, description = hourAndMinute)
-            }
-        }
-    }
-}
 
 /**
  * Prévisualisation de l'écran d'accueil.
