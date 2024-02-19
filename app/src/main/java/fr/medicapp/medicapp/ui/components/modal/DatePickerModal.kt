1f3a69f2ee7a406d4370d380bddab667169c22de
E213909K <lucas.payet@etu.univ-nantes.fr>
2024-02-19 15:17:46 +0100

    fixed crash when not selecting date in datepickerdialog

--- app/src/main/java/fr/medicapp/medicapp/ui/components/modal/DatePickerModal.kt
+++ app/src/main/java/fr/medicapp/medicapp/ui/components/modal/DatePickerModal.kt
@@ -20,7 +20,6 @@ import fr.medicapp.medicapp.ui.theme.MedicAppTheme
  * @param state L'état du DatePicker, contenant la date sélectionnée.
  * @param onDismissRequest La fonction à exécuter lorsque l'utilisateur ferme le modal.
  * @param onConfirm La fonction à exécuter lorsque l'utilisateur confirme la date sélectionnée.
- * @param colors Les couleurs du DatePicker.
  */
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
