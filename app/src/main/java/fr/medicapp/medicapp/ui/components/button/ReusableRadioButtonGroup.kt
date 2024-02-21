536f4d5688136a51348305fbed0dda1cb621009c
E213909K <lucas.payet@etu.univ-nantes.fr>
2024-02-21 17:36:33 +0100

    created ReusableRadioGroup, need to change colours

---
+++ app/src/main/java/fr/medicapp/medicapp/ui/components/button/ReusableRadioButtonGroup.kt
@@ -0,0 +1,59 @@
+package fr.medicapp.medicapp.ui.components.button
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.RadioButton
+import androidx.compose.material3.Text
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun ReusableRadioGroup(
+    modifier: Modifier = Modifier.fillMaxWidth(),
+    options: List<String>,
+    selectedOption: MutableState<String>,
+    label: String,
+) {
+    Column {
+        Text(
+            text = label,
+            modifier = modifier.padding(bottom = 8.dp),
+            color = MaterialTheme.colorScheme.onPrimary
+        )
+        options.forEach { text ->
+            Row(
+                modifier.padding(vertical = 8.dp),
+                horizontalArrangement = Arrangement.Center,
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                RadioButton(
+                    selected = (text == selectedOption.value),
+                    onClick = { selectedOption.value = text }
+                )
+                Text(
+                    text = text,
+                    modifier = modifier.padding(start = 8.dp),
+                    color = MaterialTheme.colorScheme.onPrimary
+                )
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+fun PreviewReusableRadioGroup() {
+    val selectedOption = remember { mutableStateOf("Option 1") }
+    ReusableRadioGroup(
+        options = listOf("Option 1", "Option 2", "Option 3"),
+        selectedOption = selectedOption,
+        label = "Youpi"
+    )
+}
--- app/src/main/java/fr/medicapp/medicapp/ui/screen/user/UserEditInformation.kt
+++ app/src/main/java/fr/medicapp/medicapp/ui/screen/user/UserEditInformation.kt
@@ -1,17 +1,76 @@
 package fr.medicapp.medicapp.ui.screen.user
 
+import android.os.Build
+import androidx.annotation.RequiresApi
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import fr.medicapp.medicapp.ui.components.button.ReusableOutlinedDatePickerButton
+import fr.medicapp.medicapp.ui.components.button.ReusableRadioGroup
+import fr.medicapp.medicapp.ui.components.card.ReusableElevatedCard
+import fr.medicapp.medicapp.ui.components.screen.Edit
+import fr.medicapp.medicapp.ui.components.textfield.ReusableOutlinedTextField
 import fr.medicapp.medicapp.ui.theme.EUPurpleColorShema
 import fr.medicapp.medicapp.ui.theme.MedicAppTheme
+import java.time.LocalDate
 
+@RequiresApi(Build.VERSION_CODES.O)
 @Composable
 fun UserEditInformation(
-
+    onClick: () -> Unit
 ) {
+    Edit(
+        title = "Information utilisateur",
+        bottomText = "Suivant",
+        onClick = onClick,
 
+    ) {
+        val genderState = remember { mutableStateOf("") }
+        ReusableElevatedCard {
+            Column(
+                modifier = Modifier.padding(10.dp)
+            ) {
+                ReusableOutlinedTextField(
+                    value = "",
+                    onValueChange = {},
+                    label = "Nom"
+                )
+
+                Spacer(modifier = Modifier.padding(10.dp))
+
+                ReusableOutlinedTextField(
+                    value = "",
+                    onValueChange = {},
+                    label = "Prénom"
+                )
+
+                Spacer(modifier = Modifier.padding(10.dp))
+
+                ReusableOutlinedDatePickerButton(
+                    value = LocalDate.now(),
+                    label = "Date de naissance",
+                    onSelected ={}
+                )
+
+                Spacer(modifier = Modifier.padding(10.dp))
+
+                ReusableRadioGroup(
+                    options = listOf("Homme", "Femme"),
+                    selectedOption = genderState,
+                    label = "Genre de naissance"
+                )
+            }
+        }
+    }
 }
 
+@RequiresApi(Build.VERSION_CODES.O)
 @Preview
 @Composable
 fun UserEditInformationPreview() {
@@ -20,6 +79,6 @@ fun UserEditInformationPreview() {
         dynamicColor = false,
         theme = EUPurpleColorShema
     ) {
-        UserEditInformation()
+        UserEditInformation {}
     }
 }
