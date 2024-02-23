35dedb49ebed8c97d2f9fd1d6a54ff4883ccb494
E213909K <lucas.payet@etu.univ-nantes.fr>
2024-02-23 09:58:18 +0100

    added UserEntity

---
+++ app/src/main/java/fr/medicapp/medicapp/database/entity/UserEntity.kt
@@ -0,0 +1,34 @@
+package fr.medicapp.medicapp.database.entity
+
+import io.objectbox.annotation.Entity
+import io.objectbox.annotation.Id
+import fr.medicapp.medicapp.model.User
+import fr.medicapp.medicapp.database.converter.EntityToModelMapper
+import fr.medicapp.medicapp.database.converter.LocalDateConverter
+import io.objectbox.annotation.Convert
+import java.time.LocalDate
+
+@Entity
+data class UserEntity(
+    @Id
+    var id: Long = 0L,
+    var lastName: String = "",
+    var firstName: String = "",
+    @Convert(converter = LocalDateConverter::class, dbType = String::class)
+    var birthday: LocalDate = LocalDate.now(),
+    var gender: String = "",
+    var pregnant: Boolean = false,
+    var allergies: MutableList<String>? = null
+) : EntityToModelMapper<User> {
+    override fun convert(): User {
+        return User(
+            id,
+            lastName,
+            firstName,
+            birthday,
+            gender,
+            pregnant,
+            allergies
+        )
+    }
+}
--- app/src/main/java/fr/medicapp/medicapp/model/User.kt
+++ app/src/main/java/fr/medicapp/medicapp/model/User.kt
@@ -1,12 +1,36 @@
 package fr.medicapp.medicapp.model
 
+import android.content.Context
+import fr.medicapp.medicapp.database.ObjectBox
+import fr.medicapp.medicapp.database.converter.ModelToEntityMapper
+import fr.medicapp.medicapp.database.entity.TreatmentEntity
+import fr.medicapp.medicapp.database.entity.UserEntity
 import java.time.LocalDate
 
 data class User(
+    var id: Long = 0L,
     val lastName: String = "",
     val firstName: String = "",
     val birthday: LocalDate = LocalDate.now(),
     val gender: String = "",
     val pregnant: Boolean = false,
     val allergies: MutableList<String>? = null
-)
+) : ModelToEntityMapper<UserEntity> {
+    override fun convert(context: Context): UserEntity {
+        val user =  UserEntity(
+            id,
+            lastName,
+            firstName,
+            birthday,
+            gender,
+            pregnant,
+            allergies
+        )
+        val box = ObjectBox.getInstance(context)
+        val store = box.boxFor(UserEntity::class.java)
+        store.attach(user)
+
+        return user
+
+    }
+}
