44de24320d9988c67f268ade6381076841cb5108
Lexonchik <lucaas.paayet@gmail.com>
2024-02-29 09:25:19 +0100

    added Button for searchModal

--- app/src/main/java/fr/medicapp/medicapp/ui/components/modal/SearchModal.kt
+++ app/src/main/java/fr/medicapp/medicapp/ui/components/modal/SearchModal.kt
@@ -5,10 +5,14 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ElevatedCard
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
@@ -20,10 +24,12 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import fr.medicapp.medicapp.model.OptionDialog
+import fr.medicapp.medicapp.ui.components.button.ReusableButton
 import fr.medicapp.medicapp.ui.components.textfield.ReusableOutlinedTextField
 import fr.medicapp.medicapp.ui.theme.EUYellowColorShema
 import fr.medicapp.medicapp.ui.theme.MedicAppTheme
@@ -32,12 +38,10 @@ import kotlinx.coroutines.launch
 /**
  * Cette fonction affiche une boîte de dialogue de recherche avec des options spécifiques.
  *
+ * @param title Le titre de la boîte de dialogue.
  * @param options La liste des options pour la boîte de dialogue.
- * @param cardColor La couleur des cartes dans la boîte de dialogue.
- * @param selectedCardColor La couleur de la carte sélectionnée dans la boîte de dialogue.
- * @param onDismiss La fonction à exécuter lorsque l'utilisateur ferme la boîte de dialogue.
- * @param onValidate La fonction à exécuter lorsque l'utilisateur valide une option.
- * @param preQuery La requête de recherche pré-remplie.
+ * @param onDismissRequest La fonction à appeler lorsque la boîte de dialogue est fermée.
+ * @param onConfirm La fonction à appeler lorsque l'option est confirmée.
  */
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -51,6 +55,7 @@ fun SearchModal(
     var selectedOption by remember { mutableStateOf<OptionDialog?>(null) }
     val searchResults = remember { mutableStateOf<List<OptionDialog>>(emptyList()) }
     val scope = rememberCoroutineScope()
+    val isLoading = remember { mutableStateOf(false) }
 
     AlertDialog(
         onDismissRequest = onDismissRequest,
@@ -61,55 +66,77 @@ fun SearchModal(
         },
         text = {
             Column {
+
                 ReusableOutlinedTextField(
                     value = searchQuery,
                     onValueChange = {
-                        scope.launch {
-                            searchQuery = it
-                            searchResults.value = options(it)
-                        }
+                        searchQuery = it
                     },
                     label = "",
                 )
-                Spacer(modifier = Modifier.height(10.dp))
-                LazyColumn {
-                    items(searchResults.value) { option ->
-                        ElevatedCard(
-                            onClick = {
-                                selectedOption = option
-                            },
-                            modifier = Modifier.fillMaxWidth(),
-                            colors = if (option == selectedOption) {
-                                CardDefaults.cardColors(
-                                    containerColor = MaterialTheme.colorScheme.primary,
-                                    contentColor = MaterialTheme.colorScheme.onPrimary
-                                )
-                            } else {
-                                CardDefaults.cardColors(
-                                    containerColor = MaterialTheme.colorScheme.surface,
-                                    contentColor = MaterialTheme.colorScheme.onSurface
-                                )
-                            }
-                        ) {
-                            Text(
-                                text = option.title,
-                                modifier = Modifier.padding(5.dp),
-                            )
+                Spacer(modifier = Modifier.padding(5.dp))
 
-                            option.description?.let {
-                                Text(
-                                    text = it,
-                                    modifier = Modifier.padding(5.dp),
-                                    color = if (option == selectedOption) {
-                                        MaterialTheme.colorScheme.onPrimary
-                                    } else {
-                                        MaterialTheme.colorScheme.onSurface
-                                    }
-                                )
-                            }
+                ReusableButton(
+                    text = "",
+                    icon = Icons.Filled.Search,
+                    onClick = {
+                        isLoading.value = true
+                        scope.launch {
+                            searchResults.value = options(searchQuery)
+                            isLoading.value = false
                         }
+                    }
+                )
 
-                        Spacer(modifier = Modifier.padding(10.dp))
+                Spacer(modifier = Modifier.height(10.dp))
+
+                if (isLoading.value) {
+                    CircularProgressIndicator(
+                        modifier = Modifier
+                            .size(50.dp)
+                            .padding(4.dp)
+                            .align(Alignment.CenterHorizontally)
+                    )
+                } else {
+                    LazyColumn {
+                        items(searchResults.value) { option ->
+                            ElevatedCard(
+                                onClick = {
+                                    selectedOption = option
+                                },
+                                modifier = Modifier.fillMaxWidth(),
+                                colors = if (option == selectedOption) {
+                                    CardDefaults.cardColors(
+                                        containerColor = MaterialTheme.colorScheme.primary,
+                                        contentColor = MaterialTheme.colorScheme.onPrimary
+                                    )
+                                } else {
+                                    CardDefaults.cardColors(
+                                        containerColor = MaterialTheme.colorScheme.surface,
+                                        contentColor = MaterialTheme.colorScheme.onSurface
+                                    )
+                                }
+                            ) {
+                                Text(
+                                    text = option.title,
+                                    modifier = Modifier.padding(5.dp),
+                                )
+
+                                option.description?.let {
+                                    Text(
+                                        text = it,
+                                        modifier = Modifier.padding(5.dp),
+                                        color = if (option == selectedOption) {
+                                            MaterialTheme.colorScheme.onPrimary
+                                        } else {
+                                            MaterialTheme.colorScheme.onSurface
+                                        }
+                                    )
+                                }
+                            }
+
+                            Spacer(modifier = Modifier.padding(10.dp))
+                        }
                     }
                 }
             }
